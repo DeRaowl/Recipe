e7083d28e1823e91574936640be1b266b6c9857a
Rahul M <rahulreddy93m@gmail.com>
2021-07-17 19:46:25 +0530

    Style: Refactored style

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,12 @@
 // JavaScript coming soon
+function getRandomMeal() {
+  fetch("www.themealdb.com/api/json/v1/1/random.php");
+}
+
+function getMealById(id) {
+  fetch("www.themealdb.com/api/json/v1/1/lookup.php?i=" + id);
+}
+
+function getMealBySearch(name) {
+  fetch("www.themealdb.com/api/json/v1/1/search.php?s=" + name);
+}
